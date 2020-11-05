fb28bba559a63de59d2ecda4a152fa93b45bbbe1
joscript <josua.lagat@gmail.com>
2020-11-05 09:54:35 +0800

    initialize env file and used it on db connection

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+require('dotenv/config');
 
 const app = express();
 
 
 //connect to db
-mongoose.connect('mongodb+srv://joscript:password099@rest.q1rui.mongodb.net/rest?retryWrites=true&w=majority', 
+mongoose.connect(
+    process.env.DB_CONNECTION, 
     { useNewUrlParser: true, useUnifiedTopology: true },
     () => console.log('connected to DB!')
 );
