82f16341faa05bbe9e6ebed529c89c5c0348816c
joscript <josua.lagat@gmail.com>
2020-11-05 11:49:14 +0800

    get specific user by id

--- routes/users.js
+++ routes/users.js
@@ -26,4 +26,13 @@ router.post('/', async (req, res) => {
     }
 }); 
 
+router.get('/:id', async(req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json({message: error});
+    }
+})
+
 module.exports = router;
